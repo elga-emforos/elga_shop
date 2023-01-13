0dd26769fddc04bb1ec5d6c7d37ef4aa6927bbbc
elga-emforos <122539008+elga-emforos@users.noreply.github.com>
2023-01-13 23:59:53 +0900

    20230113

--- js/plugins.js
+++ js/plugins.js
@@ -4,5 +4,6 @@ var $plugins =
 [
 {"name":"OnlineAvatar","status":true,"description":"Firebaseを使ってプレイヤーをオンライン同期します。","parameters":{"apiKey":"AIzaSyAAQ4kOP3u7CCkKFaWFsn8t6Yv5y8LQ-vc","authDomain":"elga-shop.firebaseapp.com","databaseURL":"https://elga-shop-default-rtdb.firebaseio.com","avatarEvent":"1","syncSwitchStart":"11","syncSwitchEnd":"20","syncVariableStart":"11","syncVariableEnd":"20"}},
 {"name":"TMNamePop","status":true,"description":"イベントの頭上に文字列を表示する機能を追加します。","parameters":{"backOpacity":"96","fontSize":"20","fontOutlineWidth":"4","fontOutlineColor":"rgba(0, 0, 0, 0.5)","width":"160","useRoundRect":"0","roundRectRadius":"6"}},
-{"name":"111_InputForm","status":true,"description":"フォーム作って文字入力（修正版）","parameters":{}}
+{"name":"111_InputForm","status":true,"description":"フォーム作って文字入力（修正版）","parameters":{}},
+{"name":"H2A_SkipTitle","status":true,"description":"タイトルをスキップします","parameters":{"mode":"new"}}
 ];
