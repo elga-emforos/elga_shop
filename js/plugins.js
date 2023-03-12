823055ed1ae541c23d91c7a1668402eead3aa1c4
elga-emforos <122539008+elga-emforos@users.noreply.github.com>
2023-03-12 23:56:07 +0900

    ver0.9

--- js/plugins.js
+++ js/plugins.js
@@ -5,5 +5,7 @@ var $plugins =
 {"name":"OnlineAvatar","status":true,"description":"Firebaseを使ってプレイヤーをオンライン同期します。","parameters":{"apiKey":"AIzaSyAAQ4kOP3u7CCkKFaWFsn8t6Yv5y8LQ-vc","authDomain":"elga-shop.firebaseapp.com","databaseURL":"https://elga-shop-default-rtdb.firebaseio.com","avatarEvent":"1","syncSwitchStart":"11","syncSwitchEnd":"20","syncVariableStart":"11","syncVariableEnd":"20"}},
 {"name":"TMNamePop","status":true,"description":"イベントの頭上に文字列を表示する機能を追加します。","parameters":{"backOpacity":"96","fontSize":"20","fontOutlineWidth":"4","fontOutlineColor":"rgba(0, 0, 0, 0.5)","width":"160","useRoundRect":"0","roundRectRadius":"6"}},
 {"name":"111_InputForm","status":true,"description":"フォーム作って文字入力（修正版）","parameters":{}},
-{"name":"H2A_SkipTitle","status":true,"description":"タイトルをスキップします","parameters":{"mode":"new"}}
+{"name":"H2A_SkipTitle","status":true,"description":"タイトルをスキップします","parameters":{"mode":"new"}},
+{"name":"MNKR_OpenWindowLinks","status":true,"description":"新しいウィンドウに指定されたURLを開くプラグインコマンドを追加します。","parameters":{}},
+{"name":"MPP_ChoiceEX","status":true,"description":"選択肢の機能を拡張します。","parameters":{"Max Page Row":"6","Disabled Position":"none","Choice Help Commands":"[\"ChoiceHelp\",\"<ChoiceHelp>\",\"選択肢ヘルプ\",\"<選択肢ヘルプ>\"]"}}
 ];
